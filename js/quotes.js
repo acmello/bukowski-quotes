d3958db95268f19fafc74455f1bd11d6f39ba1a7
Antonio Mello <acmello.ti@gmail.com>
2012-12-27 23:20:43 -0200

    gh-pages now is the same as master

---
+++ js/quotes.js
@@ -0,0 +1,47 @@
+window.quotes = {
+	quote1: "If you're going to try, go all the way. Otherwise dont even start" 
+		   + " This could mean losing girlfriends wives relatives and maybe even your mind" 
+		   + " It could mean not eating for three or four days It could mean freezing on a park bench"
+		   + " It could mean jail It could mean derision It could mean mockery--isolation" 
+		   + " Isolation is the gift All the others are a test of your endurance of how much you"
+		   + " Really want to do it And youll do it despite rejection and the worst odds"
+		   + " And it will be better than anything else you can imagine. If youre going to try go all the way"
+		   + " There is no other feeling like that You will be alone with the gods, and the nights will flame with fire"
+		   + " You will ride life straight to perfect laughter Its the only good fight there is",
+	
+	quote2: "For those who believe in God, most of the big questions are answered. But for those of us who can't"
+			+" readily accept the God formula, the big answers don't remain stone-written. We adjust to new conditions"
+			+" and discoveries. We are pliable. Love need not be a command nor faith a dictum. I am my own god."
+			+" We are here to unlearn the teachings of the church, state, and our educational system. We are here"
+			+" to drink beer. We are here to kill war. We are here to laugh at the odds and live our lives so well" 
+			+" that Death will tremble to take us",
+	
+	quote3: "That's the problem with drinking, I thought, as I poured myself a drink. If something bad happens you drink"
+			+" in an attempt to forget; if something good happens you drink in order to celebrate; and if nothing happens"
+			+" you drink to make something happen.",
+			
+	quote4: "there are worse things than being alone but it often takes decades to realize this and most often when you do it's too late"
+		   +" and there's nothing worse than too late",
+	
+	quote5: "I felt like crying but nothing came out. it was just a sort of sad sickness, sick sad, when you can't feel any worse. I think" 
+			+" you know it. I think everybody knows it now and then. but I think I have known it pretty often, too often.",
+			
+	quote6: "Drinking is an emotional thing. It joggles you out of the standardism of everyday life, out of everything being the same."
+			+" It yanks you out of your body and your mind and throws you against the wall. I have the feeling that drinking is a form of suicide"
+			+" where you're allowed to return to life and begin all over the next day. It's like killing yourself, and then you're reborn."
+			+" I guess I've lived about ten or fifteen thousand lives now.",
+
+	quote7: "We are Born like this Into this Into these carefully mad wars Into the sight of broken factory windows of emptiness"
+			+" Into bars where people no longer speak to each other Into fist fights that end as shootings and knifings"
+			+" Born into this Into hospitals which are so expensive that it’s cheaper to die Into lawyers who charge so much it’s"
+			+" cheaper to plead guilty Into a country where the jails are full and the madhouses closed Into a place where the masses" 
+			+" elevate fools into rich heroes",
+			
+	quote8: "Pain is strange. A cat killing a bird, a car accident, a fire.... Pain arrives, BANG, and there it is, it sits on you."
+			+" It's real. And to anybody watching, you look foolish. Like you've suddenly become an idiot. There's no cure for it"
+			+" unless you know somebody who understands how you feel, and knows how to help.",
+			
+	quote9: "This is very important -- to take leisure time. Pace is the essence. Without stopping entirely and doing nothing at all"
+			+" for great periods, you're gonna lose everything...just to do nothing at all, very, very important. And how many people"
+			+" do this in modern society? Very few. That's why they're all totally mad, frustrated, angry and hateful."
+};
